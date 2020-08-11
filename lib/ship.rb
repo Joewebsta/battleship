4f3a5697cd8fdf6703f7f8c19f427f22a0bca92b
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-11 13:52:46 -0400

    Write test - ship is not sunk by default and pass test

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,15 @@
 class Ship
   attr_reader :name, :length
-  attr_accessor :health
+  attr_accessor :health, :sunk
 
   def initialize(name, length)
     @name = name
     @length = length
     @health = length
+    @sunk = false
+  end
+
+  def sunk?
+    sunk
   end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,15 +3,23 @@ require './lib/ship'
 describe Ship do
   before { @ship = Ship.new('Cruiser', 3) }
 
-  it 'has a name' do
-    expect(@ship.name).to eql('Cruiser')
+  describe '#initialize' do
+    it 'has a name' do
+      expect(@ship.name).to eql('Cruiser')
+    end
+
+    it 'has a length' do
+      expect(@ship.length).to eql(3)
+    end
+
+    it 'has health equal to length' do
+      expect(@ship.health).to eql(@ship.length)
+    end
   end
 
-  it 'has a length' do
-    expect(@ship.length).to eql(3)
-  end
-
-  it 'has health equal to length' do
-    expect(@ship.health).to eql(@ship.length)
+  describe '#sunk?' do
+    it 'is not sunk by default' do
+      expect(@ship.sunk?).to be false
+    end
   end
 end
