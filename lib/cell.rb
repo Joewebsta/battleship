31c4720bdee4bbdc5425773a1eb67af2f5703ca3
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-11 19:26:42 -0400

    Write tests for #fire_upon. Pass tests.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,17 +1,27 @@
 class Cell
   attr_reader :coordinate
-  attr_accessor :ship
+  attr_accessor :ship, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
     @ship.nil?
   end
 
+  def fired_upon?
+    fired_upon
+  end
+
   def place_ship(ship)
     self.ship = ship
+  end
+
+  def fire_upon
+    ship.hit
+    self.fired_upon = true
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,12 +8,16 @@ describe Cell do
       expect(subject.coordinate).to eql('B4')
     end
 
-    it 'does not have ship placed within it by default' do
-      expect(subject.ship).to be_nil
+    # it 'does not contain part of a ship' do
+    #   expect(subject.ship).to be_nil
+    # end
+
+    it 'is empty' do
+      should be_empty
     end
 
-    it 'is empty by default' do
-      should be_empty
+    it 'is not hit' do
+      should_not be_fired_upon
     end
   end
 
@@ -27,6 +31,22 @@ describe Cell do
     it 'makes cell no longer empty' do
       subject.place_ship(ship)
       should_not be_empty
+    end
+  end
+
+  describe '#fire_upon' do
+    let(:ship) { Ship.new('Cruiser', 3) }
+
+    it 'decreases ship health' do
+      subject.place_ship(ship)
+      subject.fire_upon
+      expect(subject.ship.health).to eql(2)
+    end
+
+    it 'fires upon cell' do
+      subject.place_ship(ship)
+      subject.fire_upon
+      expect(subject.fired_upon).to be_true
     end
   end
 end
