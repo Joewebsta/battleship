48493ff57d9424fdaabb542e405e35f64b9e86ee
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-11 18:39:25 -0400

    Test place_ship populates cell and makes cell not empty. Pass tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -10,4 +10,8 @@ class Cell
   def empty?
     @ship.nil?
   end
+
+  def place_ship(ship)
+    self.ship = ship
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -16,4 +16,17 @@ describe Cell do
       should be_empty
     end
   end
+
+  describe '#place_ship' do
+    let(:ship) { Ship.new('Cruiser', 3) }
+
+    it 'populates cell with ship' do
+      expect(subject.place_ship(ship)).to eql(ship)
+    end
+
+    it 'makes cell no longer empty' do
+      subject.place_ship(ship)
+      should_not be_empty
+    end
+  end
 end
