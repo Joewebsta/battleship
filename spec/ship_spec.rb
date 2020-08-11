598a3279e82c13fab2776aa74a25caf2534c05ab
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-11 12:45:46 -0400

    Pass ship name and length tests

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,11 +3,11 @@ require './lib/ship'
 describe Ship do
   before { @ship = Ship.new('Cruiser', 3) }
 
-  example 'has a name' do
+  it 'has a name' do
     expect(@ship.name).to eql('Cruiser')
   end
 
-  example 'has a length' do
+  it 'has a length' do
     expect(@ship.length).to eql(3)
   end
 end
