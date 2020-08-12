3f7ffbdc3e886b019d6e061ad8968248945e63ae
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-12 16:50:20 -0400

    Create BoardCellsGenerator and create cells hash with strings

---
+++ lib/board_cells_generator.rb
@@ -0,0 +1,47 @@
+require './lib/cell'
+
+class BoardCellsGenerator
+  attr_reader :chars, :nums, :grid_cells, :rows
+  attr_accessor :cells
+
+  def initialize(rows)
+    @cells = {}
+    @rows = rows
+    @grid_cells = rows**2
+    @chars = ('A'..'Z').to_a.take(rows)
+    @nums = (1..rows).to_a
+  end
+
+  def generate
+    1.upto(grid_cells) do |cell_num|
+      coordinate = format_coordinate
+      cells[coordinate] = coordinate
+      update_coordinate(cell_num)
+    end
+
+    cells
+  end
+
+  def update_coordinate(cell_num)
+    end_of_row?(cell_num) ? update_letter_and_num : update_num
+  end
+
+  def format_coordinate
+    "#{chars.first}#{nums.first}"
+  end
+
+  def update_letter_and_num
+    "#{chars.rotate!.first}#{nums.rotate!.first}"
+  end
+
+  def update_num
+    "#{chars.first}#{nums.rotate!.first}"
+  end
+
+  def end_of_row?(cell_num)
+    (cell_num % rows).zero?
+  end
+end
+
+BCGenerator = BoardCellsGenerator.new(4)
+p BCGenerator.generate
