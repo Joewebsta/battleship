d6d408464f6198815e89ebd8beec55588ee00a21
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-13 12:24:14 -0400

    Write all tests for #valid_placement?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -25,19 +25,19 @@ describe Board do
       expect(subject.valid_coordinate?('A1')).to be true
     end
 
-    it 'ensures the last coordinate on the board is valid' do
+    it 'ensures last coordinate on the board is valid' do
       expect(subject.valid_coordinate?('D4')).to be true
     end
 
-    it 'does not allow a coordinate not on the board' do
+    it 'does not allow an invalid coordinate' do
       expect(subject.valid_coordinate?('A5')).to be false
     end
 
-    it 'does not allow another coordinate not on the board' do
+    it 'does not allow another invalid coordinate' do
       expect(subject.valid_coordinate?('E1')).to be false
     end
 
-    it 'does not allow yet another coordinate not on the board' do
+    it 'does not allow yet another invalid coordinate' do
       expect(subject.valid_coordinate?('A22')).to be false
     end
   end
@@ -46,16 +46,44 @@ describe Board do
     let(:cruiser) { Ship.new('Cruiser', 3) }
     let(:submarine) { Ship.new('Submarine', 2) }
 
-    it 'does not validate coordinates that do not equal ship length' do
+    it 'does not allow coordinates shorter than ship length' do
       expect(subject.valid_placement?(cruiser, %w[A1 A2])).to be false
     end
 
-    it 'does not validate different coordinates that do not equal ship length' do
+    it 'does not allow coordinates longer than ship length' do
       expect(subject.valid_placement?(submarine, %w[A2 A3 A4])).to be false
     end
 
-    it 'does not validate inconsecutive coordinates' do
+    it 'does not allow inconsecutive horizontal coordinates' do
       expect(subject.valid_placement?(cruiser, %w[A1 A2 A4])).to be false
+    end
+
+    it 'does not allow inconsecutive vertical coordinates' do
+      expect(subject.valid_placement?(submarine, %w[A1 C1])).to be false
+    end
+
+    it 'does not allow backwards horiztontal coordinates' do
+      expect(subject.valid_placement?(cruiser, %w[A3 A1 A1])).to be false
+    end
+
+    it 'does not allow backwards vertical coordinates' do
+      expect(subject.valid_placement?(cruiser, %w[C1 B1])).to be false
+    end
+
+    it 'does not allow diagonal coordinates' do
+      expect(subject.valid_placement?(cruiser, %w[A1 B2 C3])).to be false
+    end
+
+    it 'does not allow more diagonal coordinates' do
+      expect(subject.valid_placement?(submarine, %w[C2 D3])).to be false
+    end
+
+    it 'allows a ship with valid coordinates' do
+      expect(subject.valid_placement?(submarine, %w[A1 A2])).to be true
+    end
+
+    it 'allows another ship with valid coordinates' do
+      expect(subject.valid_placement?(cruiser, %w[B1 C1 D1])).to be true
     end
   end
 end
