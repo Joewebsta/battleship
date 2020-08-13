602ef9868d0a2b7bba37c3f4408747a23eda720e
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-13 15:13:54 -0400

    Refactor consec_cols

--- lib/board.rb
+++ lib/board.rb
@@ -13,22 +13,29 @@ class Board
 
   def valid_placement?(ship, coordinates)
     ship_length = ship.length
-    return false if ship_length != coordinates.length
-
     tot_rows = Math.sqrt(cells.length).to_i
+
+    return false if ship_length != coordinates.length
 
     valid_consec_positions = consec_rows(tot_rows, ship_length) + consec_cols(tot_rows, ship_length)
     valid_consec_positions.one? { |position| position == coordinates }
   end
 
   def consec_cols(tot_rows, ship_length)
-    consec_positions = []
-    1.upto(tot_rows) do |i|
-      columns = cells.keys.select { |cell| cell.include?(i.to_s) }
-      columns.each_cons(ship_length) { |consec_cells| consec_positions << consec_cells }
+    consec_positions = 1.upto(tot_rows).map do |i|
+      column_arrays = create_arr_of_column(i)
+      create_consec_cell_arrs(column_arrays, ship_length)
     end
 
-    consec_positions
+    consec_positions.flatten(1)
+  end
+
+  def create_arr_of_column(col_num)
+    cells.keys.select { |cell| cell.end_with?(col_num.to_s) }
+  end
+
+  def create_consec_cell_arrs(column_arrays, ship_length)
+    column_arrays.each_cons(ship_length).map { |consec_cells| consec_cells }
   end
 
   def consec_rows(tot_rows, ship_length)
